028b5dcec52753b0291995367f935a6c77a2a866
lmahdy <mahdi.mzoughi@istic.ucar.tn>
2024-11-05 18:48:38 +0100

    some comments % the audio feature :))

--- recipes/views.py
+++ recipes/views.py
@@ -39,12 +39,12 @@ def map_view(request):#this function is called when the user wants to view the m
 
 def summarize_audio(request):
     summary = ""
-    if request.method == 'POST':
+    if request.method == 'POST':# this means that the user has submitted the form
         audio_file = request.FILES.get('audio_file')
         if audio_file:
-            file_path = default_storage.save('uploads/' + audio_file.name, audio_file)
+            file_path = default_storage.save('uploads/' + audio_file.name, audio_file)#this means that the file is saved in the uploads folder in the media directory
             # Absolute path to the file for upload to Deepgram
-            full_path = os.path.join(settings.MEDIA_ROOT, file_path)
+            full_path = os.path.join(settings.MEDIA_ROOT, file_path)#this means that the full path to the file is the media root + the file path in our case its media/uploads/filename
             
             # Prepare headers and data for the Deepgram API
             headers = {
@@ -57,9 +57,9 @@ def summarize_audio(request):
                     'https://api.deepgram.com/v1/listen?summarize=v2',
                     headers=headers,
                     data=f
-                )
-                data = response.json()
-                summary = data.get('results', {}).get('summary', {}).get('short', "No summary available.")
+                )#this means that we are sending a post request to the Deepgram API with the audio file
+                data = response.json()#this means that we are converting the response to JSON format
+                summary = data.get('results', {}).get('summary', {}).get('short', "No summary available.")#this means that we are getting the summary from the response
             
             # Clean up the uploaded file if desired
             default_storage.delete(file_path)
